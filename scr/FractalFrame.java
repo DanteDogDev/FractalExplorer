bf5a0b58bba0b741c4c93e1ed82c355d4333f036
Dante Harper <dantedigdog@gmail.com>
2024-03-11 17:11:37 -0700

    got zooming and dragging to work

--- scr/FractalFrame.java
+++ scr/FractalFrame.java
@@ -5,56 +5,72 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-//keyboard/mouse event library
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+
 //Windows frame library
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-public class FractalFrame extends JFrame implements KeyListener, MouseListener {
+public class FractalFrame extends JFrame {
 
+    private FractalListener fractalListener;
     private BufferedImage canvas;
-    private int width;
-    private int height;
+    private int canvasWidth;
+    private int canvasHeight;
+    private int imageWidth;
+    private int imageHeight;
+    private int xOffset = 0;
+    private int yOffset = 0;
+    private double zoomLevel = 1.0;
     
     public FractalFrame() {
         super("Fractal Explorer");
-        setSize(width, height);
         //sets icon of the window
         Image icon = new javax.swing.ImageIcon("assets/icon.png").getImage();
         setIconImage(icon);
-        //when closing window close the program
-        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        //Add MouseListener
-        addMouseListener(this);
+
+        //Add Mouse/Key Listener
+        fractalListener = new FractalListener(this);
+        addKeyListener(fractalListener);
+        addMouseListener(fractalListener);
+        addMouseMotionListener(fractalListener);
+        addMouseWheelListener(fractalListener);
+
         //creates window
+        Toolkit toolKit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolKit.getScreenSize();
         setupFrame();
+
         //sets up the canvas
+        canvasWidth = screenSize.width;
+        canvasHeight = screenSize.height;
         setupCanvas();
         setVisible(true);
+
     }
 
     public void setupCanvas() {
         //gets size of screen
-        Toolkit toolKit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = toolKit.getScreenSize();
-        width = (int)screenSize.getWidth();
-        height = (int)screenSize.getHeight();
+        imageWidth = (int) (canvasWidth);
+        imageHeight = (int) (canvasHeight);
+
         //sets up the canvas
-        canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        canvas = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = canvas.createGraphics();
         g2d.setColor(Color.WHITE);
-        g2d.fillRect(0, 0, width, height);
+        g2d.fillRect(0, 0, imageWidth, imageHeight);
         g2d.dispose();
+
         repaint();
     }
 
     private void setupFrame() {
+        setSize(canvasWidth, canvasHeight);
+        setUndecorated(true);
+        setResizable(false);
+
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         getContentPane().setBackground(Color.black);
     }
@@ -62,65 +78,35 @@ public class FractalFrame extends JFrame implements KeyListener, MouseListener {
     @Override
     public void paint(Graphics g){
         super.paint(g);
-        //draw the image
-        g.drawImage(canvas, 0, 0,null);
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.scale(zoomLevel, zoomLevel);
+        g2D.drawImage(canvas, xOffset, yOffset,null);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        int x = e.getX();
-        int y = e.getY();
-        System.out.println("Mouse Clicked - Pixel Coordinates: (" + x + ", " + y + ")");
-
-        Graphics2D g2d = canvas.createGraphics();
-        g2d.setColor(Color.RED);
-        g2d.fillRect(x-5, y-5, 11, 11); // Draw a red rectangle
-        g2d.dispose();
+   
+    public void updateOffset(int dx, int dy) {
+        xOffset += dx/zoomLevel;
+        yOffset += dy/zoomLevel;
         repaint();
-        
+
     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    public int getXOffset() {
+        return xOffset;
+    }
+    public int getYOffset() {
+        return yOffset;
+    }
+    
+    public double getZoomLevel() {
+        return zoomLevel;
     }
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+    public void setZoomLevel(double zoom) {
+        zoomLevel = zoom;
+        repaint(); // Recreate the canvas with the new zoom level
     }
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
-    }
 
     public static void main(String[] args) {
         
---
+++ scr/FractalListener.java
@@ -0,0 +1,94 @@
+package scr;
+
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+public class FractalListener implements KeyListener, MouseListener, MouseMotionListener, MouseWheelListener{
+
+    FractalFrame frame;
+
+    public FractalListener(FractalFrame frame) {
+        this.frame = frame;
+    }
+
+    private int mouseX;
+    private int mouseY;
+
+    private Point onDrag;
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if(onDrag == null) {
+            onDrag = e.getPoint();
+        }
+        Point current = e.getPoint();
+        int dx = current.x - onDrag.x;
+        int dy = current.y - onDrag.y;
+        frame.updateOffset(dx, dy);
+        onDrag = current;
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        onDrag = e.getPoint();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        onDrag = null;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            // Close the program
+            frame.dispose(); // Close the window
+            System.exit(0); // Exit the application
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent arg0) {
+        int notches = arg0.getWheelRotation();
+        if(notches < 0){
+            frame.setZoomLevel(frame.getZoomLevel()+0.1);
+        } else {
+            frame.setZoomLevel(Math.max(0.1, frame.getZoomLevel()-0.1));
+        }
+        System.out.println(frame.getZoomLevel());
+    }
+    
+}
